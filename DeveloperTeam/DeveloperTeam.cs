bd9db24e4c1f50ab4ab38b418d4e3079df2f0a01
Trejon-Hughes <trejonhughes15@gmail.com>
2021-04-15 16:44:38 -0400

    Add project files.

---
+++ DeveloperTeam/DeveloperTeam.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperTeam
+{
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Developer> Developers { get; set; }
+
+        public Team() { }
+
+        public Team(int id, string name, List<Developer> developers)
+        {
+            ID = id;
+            Name = name;
+            Developers = developers;
+        }
+    }
+
+    public class Developer
+    {
+        public int ID { get; }
+        public string LastName { get; set; }
+        public bool PSAccountAccess { get; set; }
+
+        public Developer() { }
+
+        public Developer(int id, string lastName, bool psAccountAccess)
+        {
+            ID = id;
+            LastName = lastName;
+            PSAccountAccess = psAccountAccess;
+        }
+    }
+}
---
+++ DeveloperTeam/DeveloperTeamRepo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperTeam
+{
+    class DeveloperTeamRepo
+    {
+        List<Team> allTeams = new List<Team>();
+        public void CreateTeam(int id, string name)
+        {
+            Team team = new Team();
+            team.Name = name;
+            team.ID = id;
+            allTeams.Add(team);
+        }
+
+        public void AddMember(string team, int id, string lastName, bool psAccountAccess)
+        {
+            Developer dev = new Developer(id, lastName, psAccountAccess);
+            foreach (Team content in allTeams)
+            {
+                if (content.Name == team)
+                {
+                    content.Developers.Add(dev);
+                }
+            }
+        }
+
+        public void RemoveMember(string team, int id)
+        {
+            foreach (Team content in allTeams)
+            {
+                if (content.Name == team)
+                {
+                    foreach (Developer developer in content.Developers)
+                    {
+                        if(developer.ID == id)
+                        {
+                            content.Developers.Remove(developer);
+                        }
+                    }
+                }
+            }
+        }
+
+        public Team GetTeam(int id)
+        {
+            foreach (Team content in allTeams)
+            {
+                if (content.ID == id)
+                {
+                    return content;
+                }
+            }
+            return null;
+        }
+
+        public void RemoveTeam(int id)
+        {
+            foreach (Team content in allTeams)
+            {
+                if (content.ID == id)
+                {
+                    allTeams.Remove(content);
+                }
+            }
+        }
+    }
+}
